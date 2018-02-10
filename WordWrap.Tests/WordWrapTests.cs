acee5ca549b523636bc3059b7ed81e93e52b6d33
fernandobotelho <fernando.botelho@outlook.com>
2018-02-09 20:54:30 -0800

    Add project files.

---
+++ WordWrap.Tests/WordWrapTests.cs
@@ -0,0 +1,43 @@
+using System;
+using Xunit;
+
+namespace WordWrap.Tests
+{
+    public class WordWrapTests
+    {
+        [Theory]
+        [InlineData("This is a test", 7)]
+        [InlineData("This is a test", 8)]
+        public void Wrap_It(string input, int maxLength)
+        {
+            var actual = WordWrap.Wrap(input, maxLength);
+            Assert.Equal("This is\na test", actual);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void Wrap_EmptyOrNull_String(string value)
+        {
+            var actual = WordWrap.Wrap(value, 1);
+            Assert.Equal(value, actual);
+        }
+
+        [Fact]
+        public void Wrap_Under_Limit()
+        {
+            var expected = "Matt and Louise";
+            var actual = WordWrap.Wrap(expected, 15);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void Wrap_At_5()
+        {
+            var expected = "01234\n56789";
+            var actual = WordWrap.Wrap("01234 56789", 5);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ WordWrap/WordWrap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+
+namespace WordWrap
+{
+    public static class WordWrap
+    {
+        // Given an input string of words separated by spaces, and a maximum line length,
+        //  break the input at word boundaries such that no line is longer than the
+        //  maximum.
+        //
+        // For example, Wrap("This is a test", 7) and Wrap("This is a test", 8) should
+        //  both return "This is\na test".
+        public static string Wrap(string input, int maxLength)
+        {
+            if (string.IsNullOrWhiteSpace(input) || input.Length <= maxLength)
+            {
+                return input;
+            }
+
+            var lines = new StringBuilder();
+            var cursor = 0;
+            while (cursor + maxLength < input.Length)
+            {
+                var indexOfBlank = input.LastIndexOf(' ', cursor + maxLength);
+                int splitAt;
+                int offset;
+
+                if (indexOfBlank > cursor - 1)
+                {
+                    splitAt = indexOfBlank;
+                    offset = 1;
+                }
+                else
+                {
+                    splitAt = cursor + maxLength;
+                    offset = 0;
+                }
+
+                lines.Append(input.Substring(cursor, splitAt));
+                lines.Append("\n");
+
+                cursor = splitAt + offset;
+            }
+
+            lines.Append(input.Substring(cursor));
+            return lines.ToString();
+        }
+    }
+}
